daca74e2e94e47f9a2f92ee68e8501a32ae281d0
Jane Avriette <jane@cpan.org>
2014-04-26 09:02:55 -0400

    stub

---
+++ joshsz/challenge.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -x
+
+# determine latest ubuntu lts ami id
+
+# use public key from:
+#  https://s3.amazonaws.com/optoro-corp/opsrsrc/id_rsa.pub
+# for 'ubuntu' user
+
+# launch an m1.small instance
+
+# install this file:
+#   https://s3.amazonaws.com/optoro-corp/opsrsrc/optoro
+# into /usr/local/bin/optoro
+
+# execute that file
