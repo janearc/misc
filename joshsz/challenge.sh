1e961dd02dc1b42e06fb7a3569526622960d6e99
Jane Avriette <jane@cpan.org>
2014-04-26 14:49:20 -0400

    +notes, it's working.

--- joshsz/challenge.sh
+++ joshsz/challenge.sh
@@ -1,10 +1,46 @@
 #!/bin/sh
 
+DEBUG=1
+
+NOTES=<<NOTES
+So this script uses the aws-cli package from Amazon. You can install that
+using pip install aws-cli. You'll have to add credentials to the file
+~/.aws/config, like so:
+
+haram:joshsz jane$ cat ~/.aws/config
+[default]
+aws_access_key_id=WORDSWORDSWORDS
+aws_secret_access_key=wordsWordswordsWORDS
+# Optional, to define default region for this profile.
+region = us-east-1
+
+Please see comments a few lines down re variables that will be useful for
+you.
+
+Honestly if this got any bigger I'd switch to perl so I could have
+getopt-long or break everything into smaller shell functions (but that seems
+kind of silly). Ultimately, though, this works out to seven lines without the
+comments, vertical spacing, and variable declarations, so it seems to me like
+shell was an okay solution.
+
+The one thing it's not doing is checking to see if the security group or ssh
+keys are actually created. On balance, this doesn't seem harmful, and going
+to actually check to see if they were there creates substantially more code
+for dubious benefit.
+NOTES
+
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_AVAILABILITY_ZONE=us-east-1d
 export AMAZON_DERP_DELAY=15
 export UBUNTU_DERP_DELAY=60
-DEBUG=1
+export MY_NAME=`whoami` # not portable to everywhere
+
+# You'd change these to reflect the subnet and mask you would use for you.
+export MY_HOME=173.8.15.238
+export MY_MASK=32
+
+# You would change this to the location of your key (I only have the public
+# key, and have used my key so I can verify my work)
 SSH_KEY=~/.ssh/id_rsa
 
 # Debugging utility
@@ -17,10 +53,8 @@ print_stderr () {
 	echo $0[$$]: $* > /dev/stderr
 }
 
+# Shouldn't really be necessary.
 # set -x
-
-## Task 1
-# determine latest ubuntu lts ami id
 
 #  So, as it happens, the schema returned by aws ec2 describe-images doesn't
 #  actually include the date the ami was created, any specific keys re the OS
@@ -30,12 +64,9 @@ print_stderr () {
 #  LTS image. So we hard-code this here.
 UBUNTU_LTS_AMI_ID="ami-358c955c" # note this is 32-bit
 
-## Task 3
-# launch an m1.small instance
-
 log "creating keypair"
 aws ec2 import-key-pair \
-	--key-name jane_key \
+	--key-name ${MY_NAME}_key \
 	--public-key-material "$(cat ${SSH_KEY}.pub)"
 
 log "creating security group"
@@ -48,12 +79,12 @@ aws ec2 authorize-security-group-ingress \
 	--group-name optoro_ssh_22_inbound \
 	--protocol tcp \
 	--port 22 \
-	--cidr 173.8.15.238/32  # that's home
+	--cidr ${MY_HOME}/${MY_MASK}
 
 log "spinning up new instance"
 aws ec2 run-instances \
 	--image-id ami-358c955c \
-	--key-name jane_key \
+	--key-name ${MY_NAME}_key \
 	--instance-type m1.small \
 	--placement AvailabilityZone=$AWS_AVAILABILITY_ZONE \
 	--security-groups optoro_ssh_22_inbound | grep InstanceId | awk '{ print $2 }' | cut -d \" -f 2 | while read instance_id ; do
