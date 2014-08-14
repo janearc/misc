178900f9e98e8751d07fc5c233aa53ed9365dc05
jane avriette <jane@cpan.org>
2014-08-14 16:07:42 -0400

    maybe i need a break from this.

--- amazonfrobs/spew_policies.sh
+++ amazonfrobs/spew_policies.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-# set -x
-GN=""
-PN=""
-export GN PN
-aws iam list-groups | \
-	grep GroupName | \
-	awk '{ print $2 }' | cut -d\" -f 2 | \
-		while read gn ; do 
-			# scoping
-			GN="`echo $gn | tr -d ',:\"'`" ; export GN ;
-			echo "group $GN captured" >> /dev/stderr
-			aws iam list-group-policies --group-name $GN ; 
-		done | \
-	grep -E '^        ' | \
-		while read pn ; do 
-			# scoping
-			PN="`echo $pn | tr -d ',:\"'`" ; export PN ;
-			echo "group $GN"
-			echo "policy $PN"
-			# echo "${GN}:${PN}" ;
-		done
+TR="tr -d ',:\"'"
+GROUPS="`aws iam list-groups | grep GroupName | awk '{ print $2 }' | tr -d '"'`"
+echo $GROUPS
+for group in "$GROUPS"; do
+	POLICIES="`aws iam list-group-policies --group-name $group | grep -E '^        ' | $TR`"
+	for policy in $POLICIES; do
+		echo aws iam get-group-policy --group-name $group --policy-name $policy
+	done
+done
