84240199fe6733d81efe6652495720c7673423b0
jane avriette <jane@cpan.org>
2014-08-15 17:41:48 -0400

    add comments to describe data

--- amazonfrobs/aws_policies.js
+++ amazonfrobs/aws_policies.js
@@ -7,7 +7,35 @@ var iam = new AWS.IAM();
 // just give me all the things pls
 var params = { };
 
-iam.listGroups( params, function( err, data ) {
+iam.listGroups( params, function( err, groups ) {
 	if (err) console.log(err, err.stack); // oops
-	else console.log(data); // great success
+	else {
+		// console.log(data); // great success
+
+		/* so data looks like this:
+	
+	   [ { Path: '/',
+	       GroupName: 'NNN',
+	       GroupId: 'AXXXXXXXXXXXXXXXXXXXI',
+	       Arn: 'arn:aws:iam::1NNNNNNNNNN3:group/NNN',
+	       CreateDate: Mon Sep 30 2013 13:35:02 GMT-0400 (EDT) }, ]
+	
+		*/	
+
+		for (group in groups) {
+			// at this point we ask it to 
+			//     aws iam list-group-policies --group-name
+			// which should return to us:
+			/*
+				{
+				    "PolicyNames": [
+				        "StelligentAllowCreatePolicies", 
+				        "StelligentAllowReadPolicies", 
+				        "StelligentAllowWritePolicies", 
+				        "StelligentDenyPolicies"
+				    ]
+				}
+			*/
+
+	} 
 } );
