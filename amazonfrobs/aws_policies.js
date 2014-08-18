2a94110ef1c2e63b4f263c31922a943bc3142086
jane avriette <jane@cpan.org>
2014-08-18 13:33:27 -0400

    incremental while i sort out encoding

--- amazonfrobs/aws_policies.js
+++ amazonfrobs/aws_policies.js
@@ -5,49 +5,28 @@ AWS.config.region = 'us-east-1';
 var iam = new AWS.IAM();
 
 // just give me all the things pls
-var params = { };
+var ls_group_params = { };
 
-iam.listGroups( params, function( err, groups ) {
+iam.listGroups( ls_group_params, function( err, groups ) {
 	if (err) console.log(err, err.stack); // oops
 	else {
-		// console.log(data); // great success
-
-		/* so data looks like this:
-	
-	   [ { Path: '/',
-	       GroupName: 'NNN',
-	       GroupId: 'AXXXXXXXXXXXXXXXXXXXI',
-	       Arn: 'arn:aws:iam::1NNNNNNNNNN3:group/NNN',
-	       CreateDate: Mon Sep 30 2013 13:35:02 GMT-0400 (EDT) }, ]
-	
-		*/	
-
-		for (groupnum in groups.Groups) {
+		for (groupindex in groups.Groups) {
 			// at this point we ask it to 
 			//     aws iam list-group-policies --group-name
 			// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/IAM.html#listGroupPolicies-property
-			// which should return to us:
-			/*
-				{
-				    "PolicyNames": [
-				        "NNNNNNNN", 
-				        "NNNNNNNN", 
-				        "NNNNNNNN", 
-				        "NNNNNNNN"
-				    ]
-				}
-			*/
+			//
 			// without additional limits (marker, maxitems), this is just going to ask for 
 			// all the things.
-			var params = {
-				GroupName: groups.Groups[groupnum].GroupName
+			//
+			var ls_group_policy_params = {
+				GroupName: groups.Groups[groupindex].GroupName
 			};
-			// console.log( group );
-			// console.log("frobbing " + groups.Groups[groupnum]);
-			iam.listGroupPolicies(params, function( err, policies ) {
+
+			// console.log( "checking group policies for " + params.GroupName );
+			iam.listGroupPolicies(ls_group_policy_params, function( err, policies ) {
 				if (err) console.log(err, err.stack); // oops
 				else {
-					for (thispolicyname in policies.PolicyNames) {
+					for (policynameindex in policies.PolicyNames) {
 						// and then we need to ask it to
 						//     aws iam get-group-policy --policy-name beeboop --group-name blarp
 						// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/IAM.html#getGroupPolicy-property
@@ -56,16 +35,14 @@ iam.listGroups( params, function( err, groups ) {
 						// us from listGroupPolicies is an object, which has methods: PolicyNames IsTruncated Marker
 						// because amazon loves us so.
 						//
-						console.log( "oh look at dis " + policies.PolicyNames );
-						var params = {
-							GroupName: groups.Groups[groupnum].GroupName
-							// GroupName: groups.Groups[groupnum].GroupName,
-							// PolicyName: policies[policynum].PolicyName
+						var enumerate_policy_params = { // nomenclature here is tricky with the API re-using list_group_noun
+							GroupName: groups.Groups[groupindex].GroupName,
+							PolicyName: policies.PolicyNames[policynameindex]
 						};
-						console.log( params );
-						iam.getGroupPolicy( params, function( err, thesepolicies ) {
-							// console.log(thesepolicies); // XXX: this is null. probably we need to check where we get this value.
-						} ); // getGroupPolicy (groups, policies)
+						iam.getGroupPolicy( enumerate_policy_params, function( err, policydata ) {
+							console.log( "checking policy data for " + enumerate_policy_params.GroupName + "/" + enumerate_policy_params.PolicyName );
+							console.log(policydata);
+						} ); // getGroupPolicy (group name, policy name)
 
 					} // policies
 				}
