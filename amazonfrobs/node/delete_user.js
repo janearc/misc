c6e4e685d4fb4909793dd2993b72e710f86d3569
jane avriette <jane@cpan.org>
2014-08-20 12:30:35 -0400

    herpaderpderp

--- amazonfrobs/node/delete_user.js
+++ amazonfrobs/node/delete_user.js
@@ -9,7 +9,7 @@ export AWS_SECRET_ACCESS_KEY=`grep aws_sec ~/.aws/config | cut -d' ' -f 3`
 */
 
 
-// a quick create-user request for aws iam via node. not intended to be used in anything but the shell.
+// a quick delete-user request for aws iam via node. not intended to be used in anything but the shell.
 // jane avriette, jane@cpan.org
 //
 
@@ -25,12 +25,12 @@ var iam = new AWS.IAM();
 
 // iam offers us two params, username and path. username is required
 // because duh.
-var create_user_params = { 
+var delete_user_params = { 
 	GroupName: 'jane_test_user'
 };
 
-// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/IAM.html#createUser-property
-iam.createGroup( create_user_params, function( err, create_user_results ) {
+// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/IAM.html#deleteUser-property
+iam.deleteGroup( delete_user_params, function( err, delete_user_results ) {
 	// This means we didn't get what we wanted back. We can't do much but complain and fail.
 	//
 	if (err) { 
@@ -44,16 +44,16 @@ iam.createGroup( create_user_params, function( err, create_user_results ) {
 
 		// if it's a bare string
 		//
-		console.log( create_user_results );
+		console.log( delete_user_results );
 
 		// if it's json
 		//
-		console.log( "%j", create_user_results );
+		console.log( "%j", delete_user_results );
 
 		// if it's an encoded json string
 		//
-		console.log( decodeURIComponent( create_user_results ) );
+		console.log( decodeURIComponent( delete_user_results ) );
 
-	} // succesful result from create user
+	} // succesful result from delete user
 
-} ); // iam.createGroup
+} ); // iam.deleteGroup
