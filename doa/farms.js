8a34193394487afe6ed3db6a7c675809e5fcb24b
jane avriette <jane@cpan.org>
2014-09-09 15:25:16 -0400

    so doa has a bad habit of losing bookmarks. this should help.

---
+++ doa/farms.js
@@ -0,0 +1,54 @@
+{ farms : [
+	{
+		name : "omega shin",
+		coords : [ 405, 453 ]
+	},
+	{
+		name : "everlynn",
+		coords : [ 692, 734 ]
+	},
+	{
+		name : "stryker moore",
+		coords : [ 466, 444 ]
+	},
+	{
+		name : "ssg stewart",
+		coords : [ 395, 424 ]
+	},
+	{
+		name : "derkb15",          // LF1
+		coords : [ 472, 461 ]
+	},
+	{
+		name : "flying prickles",  // LF2
+		coords : [ 468, 422 ]
+	},
+	{
+		name : "nevertheless",     // LF3
+		coords : [ 496, 392 ]
+	},
+	{
+		name : "MCB",              // LF4
+		coords : [ 566, 371 ]
+	},
+	{
+		name : "agis mefumbie",    // LF5
+		coords : [ 464, 476 ]
+	},
+	{
+		name : "angel510",         // LF6
+		coords : [ 459, 437 ]
+	},
+	{
+		name : "rick puga",        // LF7
+		coords : [ 451, 510 ]
+	},
+	{
+		name : "dynasoar",         // LFD
+		coords : [ 483, 447 ]
+	},
+	{
+		name : "etzel",
+		coords : [ 447, 437 ]
+	}
+] };
