0c2c1769b0e4310fbc3a01614fbdd0b4e7be8810
jane avriette <jane@cpan.org>
2014-09-24 11:13:09 -0400

    derp

---
+++ nopt_usage_concat_test.js
@@ -0,0 +1,32 @@
+// test case for nopt-usage concatenation
+//
+var nopt = require('nopt')
+	, noptUsage = require('nopt-usage')
+	, Stream    = require('stream').Stream
+	, path      = require('path')
+	, knownOpts = {
+			'column_a'   : [ Boolean, null ],
+			'column_b'   : [ Boolean, null ]
+		}
+	, description = {
+			'column_a'   : 'A column about a',
+			'column_b'   : 'A column about b'
+		}
+	, defaults = {
+			'column_a'   : true,
+			'column_a'   : true
+		}
+	, shortHands = {
+			'a'          : [ '--column_a' ],
+			'b'          : [ '--column_a' ]
+		}
+	, parsed = nopt(knownOpts, process.argv)
+	, usage = noptUsage(knownOpts, shortHands, description, defaults)
+
+if (parsed['help']) {
+	// Be halpful
+	//
+	console.log( 'Usage: ' );
+	console.log( usage );
+	process.exit(0); // success
+}
