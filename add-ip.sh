943ef85718f45d5404e43cb698d8bc0491003ed2
jane arc <jane@cpan.org>
2015-02-17 14:12:35 -0500

    so geobytes is being a big jerkface.

--- add-ip.sh
+++ add-ip.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-SOURCE=$(curl -qq http://www.geobytes.com/iplocator.htm | grep 'IpAddress=' | cut -d= -f 4 | cut -d\" -f 1 | grep -E '^[0-9.]+$')
+# SOURCE=$(curl -qq http://www.geobytes.com/iplocator.htm | grep 'IpAddress=' | cut -d= -f 4 | cut -d\" -f 1 | grep -E '^[0-9.]+$')
+SOURCE=$(curl ifconfig.me)
+# Nick Muerdter: @jane: http://httpbin.org/ip or http://icanhazip.com (there are some differences in how they handle X-Forwarded-For headers, I believe.. httpbin acknowledges the header, icanhazip does not. But if you're just looking for your own IP, either should work)
+# 14:11] Aidan Feldman: @jane: you can try http://ip.jsontest.com/ or http://fugal.net/ip.cgi or http://curlmyip.com/ (edited)
 
 echo "adding ${SOURCE}/32 to sg-5ca8f939 <3"
 
